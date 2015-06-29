0b41e0331e35f03304f20eae58e66794f8b52cb0
Antti Peltonen <antti.peltonen@eniram.fi>
2015-06-29 13:05:33 +0300

    Releasing 0.1.0. Bumbing up the version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='redisjobqueue',
-	version='0.0.2',
+	version='0.1.0',
 	packages=find_packages(),
 	description='Non-blocking job queue for Redis backend with distributed locking mechanism to prevent double work.',
 	author='Antti Peltonen',
